7dd27d9f144c11185769f55d4cae5253bac75c09
Simon Sekavcnik <simon.sekavcnik@gmail.com>
2022-11-21 14:03:13 +0100

    RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@ import subprocess, os
 # Doxygen
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 if read_the_docs_build:
-     subprocess.call('cd ../doxygen; doxygen', shell=True)
+     subprocess.call('cd .. ; doxygen', shell=True)
 
 
 # -- Project information -----------------------------------------------------
