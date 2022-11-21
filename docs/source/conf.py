180b1ca5de96f38fac70244a71aa3887616741ed
Simon Sekavcnik <simon.sekavcnik@gmail.com>
2022-11-21 13:38:42 +0100

    RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,6 +2,10 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import subprocess, os
+
+# Doxygen
+subprocess.call('doxygen Doxyfile.in', shell=True)
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -14,7 +18,19 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx_sitemap',
+    'sphinx.ext.inheritance_diagram',
+    'breathe'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +40,13 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# -- Breathe configuration -------------------------------------------------
+
+breathe_projects = {
+	"Generic Netlink Asymetric Communication Module": "../doxygen/xml/"
+}
+breathe_default_project = "Generic Netlink Asymetric Communication Module"
+breathe_default_members = ('members', 'undoc-members')
